aa12dcbf61147047963d15866179cc8df70343bd
Collins Joe <khodelhord@gmail.com>
2024-12-08 19:20:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 // Add this before defining your routes
 app.use(
     cors({
-      origin: 'http://localhost:5173', // Allow requests from this origin
+      origin: 'https://forms-xtocast.netlify.app/', // Allow requests from this origin
       methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'], // Allowed methods
       allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
       credentials: true, // Allow cookies and credentials
